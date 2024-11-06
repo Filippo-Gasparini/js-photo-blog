da6f66b2b145196045e1de18616e2df57b6d8ac5
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-06 14:21:02 +0100

    ADD: genero le foto

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ console.log('posts')
 
 const postCards = document.querySelector('posts') 
 
-function fetchPhotos(root, n = 6) {  
+function fetchPhotos( n = 6) {  
 for (let i = 0; i <= n; i++)
     axios
     .get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     .then ((res) => {
-        console.log(res.data)
+        const post = res.data
+        console.log(post)
     })
     .catch ((err)=> {
         console.log(err)
@@ -18,4 +19,6 @@ for (let i = 0; i <= n; i++)
 }
 
 
-//fetchPhotos(postCards)
+
+fetchPhotos(postCards) 
+    
