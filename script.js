cb8e28c318d114910f44d72331c4665efd0b960d
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-06 15:20:04 +0100

    ADD: correzione

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ console.log('posts')
 
 
 
-const postCards = document.querySelector('posts') 
+const postCards = document.querySelector('card') 
 
 function fetchPhotos( n = 6) {  
-for (let i = 0; i <= n; i++)
+for (let i = 0; i < n; i++)
     axios
     .get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     .then ((res) => {
         const post = res.data
         console.log(post)
+        //appendPost (post, postCards)
+        //console.log(appendPost) 
     })
-    .catch ((err)=> {
+    .catch ((err) => {
         console.log(err)
     }) 
 }
 
 
 
-fetchPhotos(postCards) 
-    
+const foto = fetchPhotos() 
+
+
+/*function appendPost(){
+    console.log(post)
+
+    postCards.forEach((post) => {
+        console.log(postCards)
+    })
+}/** */
